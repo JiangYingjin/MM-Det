3980bcba28e12bc1cb7435edafb9ba2453927874
Jiangyj.Tech <Chief@Jiangyj.Tech>
2024-12-17 15:56:11 +0800

    Refactor imports and improve code formatting across multiple files; update module paths to LLaVA structure. Enhance readability by standardizing argument formatting and ensuring consistent use of string literals. This includes changes in `prepare_mm_representations.py`, `mm_utils.py`, `utils.py`, and various model files.

--- LLaVA/scripts/merge_lora_weights.py
+++ LLaVA/scripts/merge_lora_weights.py
@@ -1,11 +1,13 @@
 import argparse
-from llava.model.builder import load_pretrained_model
-from llava.mm_utils import get_model_name_from_path
+from LLaVA.llava.model.builder import load_pretrained_model
+from LLaVA.llava.mm_utils import get_model_name_from_path
 
 
 def merge_lora(args):
     model_name = get_model_name_from_path(args.model_path)
-    tokenizer, model, image_processor, context_len = load_pretrained_model(args.model_path, args.model_base, model_name, device_map='cpu')
+    tokenizer, model, image_processor, context_len = load_pretrained_model(
+        args.model_path, args.model_base, model_name, device_map="cpu"
+    )
 
     model.save_pretrained(args.save_model_path)
     tokenizer.save_pretrained(args.save_model_path)
