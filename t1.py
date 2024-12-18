f1804451dfcc347ef31fd3d5599befc496fc4921
Jiang Yingjin <i@JYJ.cx>
2024-12-18 09:08:53 +0000

    Update feature extraction script in `t1.py` to load features from a new path and enhance output formatting. The script now prints feature shapes for the first and eighth frames, improving clarity in feature information display. This change facilitates better debugging and understanding of the feature extraction process.

--- t1.py
+++ t1.py
@@ -2,12 +2,20 @@
 import torch
 
 # 从文件中读取特征
-features = torch.load("out/mm_feat1.pt")
+features = torch.load(
+    "/U_20240905_ZSH_SMIL/jyj/proj/MM-Det/features/mm/genvideo/val/real/MSR-VTT/MSRVTT_400.pt"
+)
 
-# 打印特征信息
-print("视觉特征 shape:", features["visual"].shape)
-print("\n文本特征信息:")
-for layer, feat in features["textual"].items():
-    print(f"- Layer {layer} shape:", feat.shape)
+# 打印特征尺寸
+print("特征信息:")
+print("第1帧:")
+print("- 视觉特征 shape:", features[1]["visual"].shape)
+print("- 文本特征 shape:")
+for layer, feat in features[1]["textual"].items():
+    print(f"  Layer {layer}:", feat.shape)
 
-print(features)
+print("\n第8帧:")
+print("- 视觉特征 shape:", features[8]["visual"].shape)
+print("- 文本特征 shape:")
+for layer, feat in features[8]["textual"].items():
+    print(f"  Layer {layer}:", feat.shape)
